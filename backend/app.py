b7d456481a12e1fdbf4270cd691d7331d73dcb28
Hassham-Ahmed <hasshamahmed73@gmail.com>
2026-09-18 00:00:37 +0500

    Fix get_db to use environment variables for Aiven MySQL

--- backend/app.py
+++ backend/app.py
@@ -42,13 +42,13 @@ if __name__ == "__main__":
     app.run(debug=True)
 
 
-def get_db_connection():
+def get_db():
     return mysql.connector.connect(
         host=os.getenv("DB_HOST"),
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD"),
         database=os.getenv("DB_NAME"),
-        port=int(os.getenv("DB_PORT", 3306)),
+        port=int(os.getenv("DB_PORT", 28028)),
         ssl_verify_identity=False,
         ssl_disabled=False
     )
